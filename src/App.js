e70c06cce46655275e2f99f59971e00165dbe2dc
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-11 15:58:03 +0530

    create react project for my location and want to go location

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+
+
+const App = () => {
+
+  return (
+   <div className='App'>
+<iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=panadura%20sri%20lanka&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+<h1>Hi all</h1>
+   </div>
+  );
+};
+
+export default App;
